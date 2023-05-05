309f58f139d756b4ec56d251083857f21aed29fb
MUHAMMAD ADHIEM WICAKSANA <didanc15@gmail.com>
2023-05-06 06:54:02 +0700

    update models

---
+++ models/user.go
@@ -0,0 +1,15 @@
+package main // or your package name
+
+import (
+    "gorm.io/gorm"
+)
+
+
+type User struct {
+	gorm.Model
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Address     string `json:"address"`
+}
