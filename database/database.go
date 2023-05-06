78c92b30807e96bba1c712bab3930e116883a66d
MUHAMMAD ADHIEM WICAKSANA <102276858+madhiemw@users.noreply.github.com>
2023-05-06 08:56:56 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -7,7 +7,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitDB() *gorm.DB {
+func ConnectDB() *gorm.DB {
 	dsn := "root:44mkcrZP7F3sK2t81XSv@tcp(containers-us-west-124.railway.app:6014)/railway?parseTime=true"
 	// dsn := "root:44mkcrZP7F3sK2t81XSv@tcp(localhost:3306)/futsal?parseTime=true"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
