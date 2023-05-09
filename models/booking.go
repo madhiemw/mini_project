fd68cd64b737d0c854b604c926bac63cfef84763
MUHAMMAD ADHIEM WICAKSANA <102276858+madhiemw@users.noreply.github.com>
2023-05-09 18:20:10 +0700

    update

--- models/booking.go
+++ models/booking.go
@@ -6,4 +6,11 @@ import (
 
 type booking struct{
 	gorm.Model
+	userID			int `json:"user_id"`
+	FieldID			int `json:"field_id"`
+	BookingDate		string `json:"booking_date"`
+	BookingHours	int `json:"booking_hours"`
+	StartTime		string `json:"username"`
+	EndTime			int `json:""`
+	Confirmed		bool `json:"confirmed"`
 }
