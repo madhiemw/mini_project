a557f540ca207f342fc9f379f829920142d024ce
MUHAMMAD ADHIEM WICAKSANA <102276858+madhiemw@users.noreply.github.com>
2023-05-10 16:40:03 +0700

    update

--- models/schedules.go
+++ models/schedules.go
@@ -7,8 +7,8 @@ import (
 
 type Schedules struct{
 	gorm.Model
-	AdminID   	int   `json:"admin_id"`
+	AdminID   			int   	`json:"admin_id"`
 	SchedulesDay        string `json:"schedules_day"`
 	SchedulesTimeStart  int `json:"schedules_time_start"`
-	SchedulesTimeEnd  int `json:"schedules_time_start"`
+	SchedulesTimeEnd  	int `json:"schedules_time_end"`
 }
